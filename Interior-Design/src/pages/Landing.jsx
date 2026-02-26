760eb7525c393b4b0ebec639267af949be8a0e5a
brenda njoki <ngunjiribrenda5@gmail.com>
2026-02-26 16:36:40 +0300

    Adds a backround image

---
+++ Interior-Design/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+function Landing(){
+    return (
+        <div className="relative h-screen w-full">
+            <div className="absolute inset-0 bg-coer bg-center" style={{backgoundImage: "https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW50ZXJpb3J8ZW58MHx8MHx8fDA%3D"}}></div>
+            
+        </div>
+    )
+}
